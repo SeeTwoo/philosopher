4e881c30ad5653ba359e343ba04b1816c1359fc0
Walter Beschon <wbeschon@f6r8s14.clusters.42paris.fr>
2025-07-28 17:32:20 +0200

    no hope anymore

--- src/monitor_threads.c
+++ src/monitor_threads.c
@@ -6,7 +6,7 @@
 /*   By: wbeschon <wbeschon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 12:57:16 by wbeschon          #+#    #+#             */
-/*   Updated: 2025/07/28 16:42:31 by wbeschon         ###   ########.fr       */
+/*   Updated: 2025/07/28 17:27:33 by wbeschon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,26 @@ void	monitor_threads(t_sim *sim)
 	int		i;
 	t_philo	*philos;
 
-	i = 0;
 	philos = sim->philos;
 	while (1)
 	{
-		pthread_mutex_lock(&philos[i].meal_mutex);
-		if (get_time_ms() - philos[i].last_meal_time > sim->ttd)
+		i = 0;
+		while (i < sim->nb_ph)
 		{
-			pthread_mutex_lock(&sim->death_mutex);
-			sim->someone_died = 1;
-			pthread_mutex_unlock(&sim->death_mutex);
-			pthread_mutex_lock(&sim->write_mutex);
-			printf("%ld philo number %d %s\n", get_time_ms() - sim->start_time, i, DIED);
-			break ;
+			pthread_mutex_lock(&philos[i].meal_mutex);
+			if (get_time_ms() - philos[i].last_meal_time > sim->ttd)
+			{
+				pthread_mutex_lock(&sim->death_mutex);
+				sim->someone_died = 1;
+				pthread_mutex_unlock(&sim->death_mutex);
+				pthread_mutex_lock(&sim->write_mutex);
+				printf("%ld philo number %d %s\n", get_time_ms() - sim->start_time, i, DIED);
+				pthread_mutex_unlock(&sim->write_mutex);
+				break ;
+			}
+			pthread_mutex_unlock(&philos[i].meal_mutex);
+			i++;
 		}
-		pthread_mutex_unlock(&philos[i].meal_mutex);
-		i++;
-		if (i == sim->nb_ph)
-			i = 0;
+		usleep(500);
 	}
 }
