803c157599d2f0c63e6528b2e7fb35c9b822d5ac
SeeTwoo <walti.bee@gmail.com>
2025-07-29 22:32:29 +0200

    added a wait start time for the lone philo

--- philo/src/lone_philo_runtime.c
+++ philo/src/lone_philo_runtime.c
@@ -6,7 +6,7 @@
 /*   By: wbeschon <wbeschon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:13:05 by wbeschon          #+#    #+#             */
-/*   Updated: 2025/07/28 18:15:09 by wbeschon         ###   ########.fr       */
+/*   Updated: 2025/07/29 22:10:05 by seetwoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	*lone_philo_runtime(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	wait_for_start(philo);
 	phlog(philo, FORK);
 	ft_sleep(philo->sim->ttd, philo->sim);
 	return (NULL);
